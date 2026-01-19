0d0ddb996143a8a4c2c04fc0b3a6fbbc08a85f33
lwu <wulei@airia.cn>
2026-01-19 19:53:09 +0800

    feat:Add some OD sample

---
+++ HuaweiOD/DoubleC/106.cpp
@@ -0,0 +1,87 @@
+//
+// Created by 巫妖王 on 2026/1/19.
+//
+
+/*
+题目描述
+
+现有一组砝码，重量互不相等，分别为m1,m2,m3…mn；
+每种砝码对应的数量为x1,x2,x3...xn。现在要用这些砝码去称物体的重量，问能称出多少中不同的重量。
+
+注：
+
+称重重量包括0
+
+输入描述:
+
+输入包含多组测试数据。
+对于每组测试数据：
+第一行：n --- 砝码种数(范围[1,10])
+第二行：m1 m2 m3 ... mn --- 每种砝码的重量(范围[1,2000])
+第三行：x1 x2 x3 .... xn --- 每种砝码的数量(范围[1,6])
+
+输出描述:
+
+利用给定的砝码可以称出的不同的重量数
+
+示例1
+
+输入
+
+2
+
+1 2
+
+2 1
+
+输出
+
+5
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+  vector<int> m(n), x(n);
+  for (int i = 0; i < n; i++) cin >> m[i];
+  for (int i = 0; i < n; i++) cin >> x[i];
+
+  int maxSum = 0;
+  for (int i = 0; i < n; i++) {
+    maxSum += m[i] * x[i];
+  }
+
+  vector<char> dp(maxSum + 1, 0);
+  dp[0] = 1;  // 重量 0 一定可以
+
+  for (int i = 0; i < n; i++) {
+    int weight = m[i];
+    int cnt = x[i];
+
+    // 多重背包：逆序枚举
+    for (int s = maxSum; s >= 0; s--) {
+      if (!dp[s]) continue;
+      for (int k = 1; k <= cnt; k++) {
+        int ns = s + k * weight;
+        if (ns > maxSum) break;
+        dp[ns] = 1;
+      }
+    }
+  }
+
+  int ans = 0;
+  for (int i = 0; i <= maxSum; i++) {
+    if (dp[i]) ans++;
+  }
+
+  cout << ans << "\n";
+
+  return 0;
+}
---
+++ HuaweiOD/DoubleC/108.cpp
@@ -0,0 +1,63 @@
+//
+// Created by 巫妖王 on 2026/1/19.
+//
+
+/*
+给定一个非空数组(列表)
+起元素数据类型为整型
+请按照数组元素十进制最低位从小到大进行排序
+十进制最低位相同的元素，相对位置保持不变
+当数组元素为负值时，十进制最低为等同于去除符号位后对应十进制值最低位
+
+输入描述
+给定一个非空数组(列表)
+其元素数据类型为32位有符号整数
+数组长度为[1,1000]
+输出描述
+
+排序后的数组
+
+示例1：
+
+输入
+1,2,5,-21,22,11,55,-101,42,8,7,32
+输出
+1,-21,11,-101,2,22,42,32,5,55,7,8
+*/
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+vector<int> parseArray(const string& line, char delim) {
+  vector<int> array;
+  stringstream ss(line);
+  string item;
+  while (getline(ss, item, delim)) {
+    array.push_back(stoi(item));
+  }
+  return array;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string line;
+  getline(cin, line);
+
+  auto a = parseArray(line, ',');
+
+  stable_sort(a.begin(), a.end(), [](int x, int y) {
+      return abs(x) % 10 < abs(y) % 10;
+  });
+
+  for (int i = 0; i < a.size(); i++) {
+    if (i) cout << ",";
+    cout << a[i];
+  }
+  cout << "\n";
+
+  return 0;
+}
